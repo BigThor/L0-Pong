c26de8696b066c21fee0c2be48b844d44f263fcb
Víctor Adrián Suarez Ruiz <jr._5oct1998@hotmail.com>
2021-07-26 14:30:11 -0500

    Add score functionality

--- Ball.lua
+++ Ball.lua
@@ -10,6 +10,11 @@ function Ball:init(x, y, side)
 end
 
 function Ball:update(dt)
+    -- ball is going to touch top or bottom side
+    if self.y < 0 or self.y + self.side > VIRTUAL_HEIGHT  then
+        self.dy = -self.dy
+    end
+
     self.x = self.x + self.dx * dt
     self.y = self.y + self.dy * dt
 end
@@ -27,4 +32,30 @@ function Ball:reset()
     
     self.dx = math.random(2) == 1 and 120 or -120
     self.dy = math.random(-50, 50)
+end
+
+function Ball:isColliding(paddle)
+
+    -- either ball x is outside (greater) of paddle's right side 
+    --   or paddle x is outside (greater) of ball's right side
+    if self.x > paddle.x + paddle.width or paddle.x > self.x + self.side then
+        return false
+    end
+
+    -- either ball y is outside (greater) of paddle's bottom side
+    --   or paddle y is outside (greater) of ball's bottom side
+    if self.y > paddle.y + paddle.height or paddle.y > self.y + self.side then
+        return false
+    end
+
+    -- they are colliding
+    return true
+end
+
+function Ball:randomizeYSpeed()
+    if self.dy < 0 then 
+        self.dy = -math.random (10, 150)
+    else
+        self.dy = math.random (10, 150)
+    end
 end
--- main.lua
+++ main.lua
@@ -26,12 +26,16 @@ PADDLE_SPEED = 200
 
 -- Ball constants
 BALL_SIDE = 4
+SPEED_INCREMENT = 1.05
 
 --[[
     Runs when the game first starts up, only once;
     used to initialize the game
 ]]
 function love.load()
+    -- set game title
+    love.window.setTitle('Big Pong')
+
     -- prevents pixels to become blurry
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
@@ -39,7 +43,7 @@ function love.load()
     math.randomseed(os.time())
 
     -- fonts
-    smallFont = love.graphics.newFont('retro_gaming.ttf', 8);
+    smallFont = love.graphics.newFont('retro_gaming.ttf', 11);
     mediumFont = love.graphics.newFont('retro_gaming.ttf', 16);
     bigFont = love.graphics.newFont('retro_gaming.ttf', 24);
 
@@ -48,11 +52,7 @@ function love.load()
         resizable = false,
         vsync = true
     })
-
-    -- initialize score variables
-    player1Score = 0
-    player2Score = 0
-
+    
     -- initialize ball
     ball = Ball((VIRTUAL_WIDTH - BALL_SIDE) / 2, (VIRTUAL_HEIGHT - BALL_SIDE) / 2,
         BALL_SIDE, BALL_SIDE)
@@ -61,6 +61,14 @@ function love.load()
     paddleP1 = Paddle(10, 30, PADDLE_WIDTH, PADDLE_HEIGHT)
     paddleP2 = Paddle(VIRTUAL_WIDTH - PADDLE_WIDTH - 10, 
         VIRTUAL_HEIGHT - PADDLE_HEIGHT - 30, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+    -- initialize score variables
+    player1Score = 0
+    player2Score = 0
+    servingPlayer = 1
+
+    -- initialize gamestate
+    gameState = 'start'
 end
 
 --[[
@@ -73,10 +81,10 @@ function love.keypressed(key)
         love.event.quit()
     elseif key == 'enter' or key == 'return' then
         if gameState == 'start' then
-            gameState = 'play'
-        else
-            gameState = 'start'
             ball:reset()
+            gameState = 'serve'
+        elseif gameState == 'serve' then
+            gameState = 'play'
         end
     end
 end
@@ -88,27 +96,15 @@ end
 function love.draw()
     push:start()
 
-    -- display title
-    love.graphics.setFont(smallFont)
-    love.graphics.printf(
-        'Hello Pong!',
-        0, -- X
-        10, -- Y
-        VIRTUAL_WIDTH, -- disponible width for display
-        'center' -- alignment
-    )
+    displayTitle()
+    displayFPS()
+    displayScore()
 
-    -- display score
-    love.graphics.setFont(bigFont)
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50,
-        VIRTUAL_HEIGHT / 3)
-    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 25,
-        VIRTUAL_HEIGHT / 3)
-
+    -- set white color for paddles and ball
+    love.graphics.setColor(255, 255, 255, 255)
     -- draw players paddles
     paddleP1:render()
     paddleP2:render()
-
     -- draw ball
     ball:render()
 
@@ -122,6 +118,43 @@ end
     frame rates
 ]]
 function love.update(dt)
+    -- move the ball only when gamestate is play
+    if gameState == 'play' then
+
+        if ball:isColliding(paddleP1) then
+            ball.dx = -ball.dx * SPEED_INCREMENT
+            ball.x = paddleP1.x + PADDLE_WIDTH
+
+            -- keep velocity the same direction, but random angle
+            ball:randomizeYSpeed()
+        end
+        
+        if ball:isColliding(paddleP2) then
+            ball.dx = -ball.dx * SPEED_INCREMENT
+            ball.x = paddleP2.x - BALL_SIDE
+
+            -- keep velocity the same direction, but random angle
+            ball:randomizeYSpeed()
+        end
+        
+        ball:update(dt)
+    end
+
+    -- ball scored left
+    if ball.x - ball.side < 0 then
+        servingPlayer = 1
+        player2Score = player2Score + 1
+        ball:reset()
+        gameState = 'serve'
+    end
+    -- ball scored right
+    if ball.x > VIRTUAL_WIDTH then
+        servingPlayer = 2
+        player1Score = player1Score + 1
+        ball:reset()
+        gameState = 'serve'
+    end
+
     -- Player 1 controls
     if love.keyboard.isDown('w') then
         paddleP1.dy = -PADDLE_SPEED
@@ -144,8 +177,54 @@ function love.update(dt)
     paddleP1:update(dt)
     paddleP2:update(dt)
 
-    -- move the ball only when gamestate is play
-    if gameState == 'play' then
-        ball:update(dt)
+end
+
+--[[
+    Render FPS, because everyone wants to know them 
+]]
+function displayFPS()
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(0, 255, 0, 255)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
+end
+
+--[[
+    Display score of both players
+]]
+function displayScore()
+    love.graphics.setFont(bigFont)
+    love.graphics.setColor(255, 255, 255, 255)
+
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50,
+        VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30,
+        VIRTUAL_HEIGHT / 3)
+
+end
+
+--[[
+    Display a different title depending on game state
+]]
+function displayTitle()
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(255, 255, 255, 255)
+
+    -- display title
+    if gameState == 'start' then
+        love.graphics.printf(
+            'Welcome to Pong!\nPress Enter to begin',
+            0, -- X
+            10, -- Y
+            VIRTUAL_WIDTH, -- disponible width for display
+            'center' -- alignment
+        )
+    elseif gameState == 'serve' then
+        love.graphics.printf(
+            'Player 1 serve!\nPress Enter to shoot',
+            0, -- X
+            10, -- Y
+            VIRTUAL_WIDTH, -- disponible width for display
+            'center' -- alignment
+        )
     end
 end
